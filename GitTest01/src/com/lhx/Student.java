461b513795a61d099bca48f6fe34070584beadc0
java <3269811681@qq.com>
2022-06-20 20:09:42 +0800

    第二次提交，在Student中添加了age，sex，height属性，以及setter和getter方法，空参数构造器，有参数构造器

--- GitTest01/src/com/lhx/Student.java
+++ GitTest01/src/com/lhx/Student.java
@@ -1,4 +1,41 @@
 package com.lhx;
 
 public class Student {
+    private int age;
+    private double height;
+    private String sex;
+
+    public Student() {
+
+    }
+
+    public Student(int age, double height, String sex) {
+        this.age = age;
+        this.height = height;
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
